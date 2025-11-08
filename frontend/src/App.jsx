432c4ae39dba9bf7926013fd05326387b972fb77
JoaoRobert0 <devjoaoroberto@gmail.com>
2025-11-08 04:46:34 +0000

    feat: set up initial project with Vite and Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center text-blue-600">
+        Hello using Tailwind CSS and Vite!
+      </h1>
+
+    </>
+  )
+}
+
+export default App
